b6e1bc77db41fa5b9314a522756524247667c961
ram_tanniru <ramtanniru2@gmail.com>
2024-11-06 01:41:49 +0530

    constants

---
+++ src/utils/data.js
@@ -0,0 +1,115 @@
+export const cards = [
+    {
+      src : 'shop',
+      price : '10.5 k',
+      desc: 'Sallers active our site'
+    },
+    {
+      src : 'money',
+      price : '33 k',
+      desc: 'Mopnthly Produduct Sale'
+    },
+    {
+      src : 'bag',
+      price : '45.5 k',
+      desc: 'Customer active in our site'
+    },
+    {
+      src : 'moneyBag',
+      price : '25 k',
+      desc: 'Anual gross sale in our site'
+    }
+];
+
+export const profiles = [
+    {
+      src : 'user1',
+      name : 'Tom Cruise',
+      designation : 'Founder & Chairman',
+    },
+    {
+      src : 'user2',
+      name : 'Emma Watson',
+      designation : 'Managing Director',
+    },
+    {
+      src : 'user3',
+      name : 'Will Smith',
+      designation : 'Product Designer',
+    }
+];
+
+export const services = [
+    {
+      src : 'bus',
+      name : 'FREE AND FAST DELIVERY',
+      desc : 'Free delivery for all orders over $140'
+    },
+    {
+      src : 'support',
+      name : '24/7 CUSTOMER SERVICE',
+      desc : 'Friendly 24/7 customer support'
+    },
+    {
+      src : 'safety',
+      name : 'MONEY BACK GUARANTEE',
+      desc : 'We reurn money within 30 days'
+    }
+]
+
+export const categoryData = [
+  {
+    category : "Woman’s Fashion"
+  },
+  {
+    category : "Men’s Fashionn"
+  },
+  {
+    category : "Electronics"
+  },
+  {
+    category : "Home & Lifestyle"
+  },
+  {
+    category : "Medicine"
+  },
+  {
+    category : "Sports & Outdoor"
+  },
+  {
+    category : "Baby’s & Toys"
+  },
+  {
+    category : "Groceries & Pets"
+  },
+  {
+    category : "Health & Beauty"
+  }
+]
+
+export const tempCategory = [
+  {
+    src : '/category/phone.svg',
+    title : 'Phone'
+  },
+  {
+    src : '/category/computer.svg',
+    title : 'Computers'
+  },
+  {
+    src : '/category/watch.png',
+    title : 'Smart Watch'
+  },
+  {
+    src : '/category/camera.svg',
+    title : 'Camera'
+  },
+  {
+    src : '/category/headphones.svg',
+    title : 'HeadPhones'
+  },
+  {
+    src : '/category/gaming.svg',
+    title : 'Gaming'
+  },
+]
